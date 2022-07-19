8ce32954a5b614283051a32ecec0faf54e372ea9
mengyi <yiii9292@gmail.com>
2022-07-19 13:19:40 -0400

    deploy to Chrome

--- index.js
+++ index.js
@@ -2,20 +2,44 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+// get the leads from localstorage and store it
+const leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  renderLeads();
-  //  remove input value every time after user inputs
-  inputEl.value = "";
-  console.log(myLeads);
+// get the data if it exists in localstorage
+if (leadsFromLocal) {
+  myLeads = leadsFromLocal;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // save to local storage
+    // add to array and render
+    // since only one tab should be active and in the current window
+    // the return variable should only have one entry
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+    render(myLeads);
+  });
 });
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     // create HTML element with JS
     // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+    // if this is just a list string as a link but link target
+    // does not specified
+    // listItems += "<li><a href='#'>"+ myLeads[i] + "</a></li>"
+
+    // if the link targe is specified
+    // listItems += "<li><a href='
+    // " + myLeads[i] + "
+    // '>"+ myLeads[i] + "</a></li>"
 
     // const li = document.createElement("li");
     // li.textContent = myLeads[i];
@@ -23,22 +47,31 @@ function renderLeads() {
 
     // or to render them altogether since manipulation DOM costs..
     // listItems +=
-    //   "<li><a target='_blank' href='" +
-    //   myLeads[i] +
-    //   "'>" +
-    //   myLeads[i] +
-    //   "</li>";
+    //  "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] +
+    //  "</li>";
 
+    // let's save the trouble to make single and double quotes by template tag
     listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
+        <li>&nbsp;
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
             </a>
         </li>`;
-
-    // fdsafsdafsadf
-
-    console.log(listItems);
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  // remove input value every time after user inputs
+  inputEl.value = "";
+  // save value to local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
